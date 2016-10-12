57bc587f580d74c7e4f5f4b0bf40d4e683e2ea73
Douglas Silva <douglaseusilva@gmail.com>
2016-10-12 01:04:02 -0300

    Implementação de hash estático. Testada a leitura do arquivo dublin.txt.

---
+++ src/example/StaticHashFileExample.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+import field.DoubleField;
+import field.IntegerField;
+import field.StringField;
+import file.Record;
+import file.StaticHashFile;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import statistics.DefaultStatisticCenter;
+
+/**
+ *
+ * @author douglas
+ */
+public class StaticHashFileExample {
+ 
+    public static void print(Iterator<Record> it) {
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+    
+    public static void dublin() throws IOException {
+        int qtdBuckets = 5;
+        int pageSize = 4096;
+        
+        StaticHashFile file = new StaticHashFile(new DefaultStatisticCenter(),
+                "dublinObjectsSHF.hfl", pageSize, qtdBuckets,
+                new IntegerField("id"),
+                new DoubleField("lat"),
+                new DoubleField("lgt"),
+                new StringField("title", 102)); //can store titles with 50 characters
+
+        file.open();
+        if (file.length() == 0) { //load the file
+             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("dublin.txt")));
+                                    
+            int id, p1, p2;
+            double lat, lgt;
+            String title;
+            
+            String line = reader.readLine();
+            while(line!=null){
+                p1= line.indexOf(' '); 
+                id = Integer.parseInt(line.substring(0,p1)); //read the id
+                
+                p2= line.indexOf(' ', p1+1); //discard the second id
+                
+                p1=p2+1;p2= line.indexOf(' ', p1);
+                lat = Double.parseDouble(line.substring(p1,p2));  //read the latitude
+                                
+                p1=p2+1;p2= line.indexOf(' ', p1);
+                lgt = Double.parseDouble(line.substring(p1,p2));  //read the longitude
+                
+                title = line.substring(p2+1).trim(); 
+                title = title.length()>50 ? title.substring(0,50) : title; //get the first 50 characters of the title
+                                
+                file.insert(file.createRecord(id, lat, lgt, title));
+                
+                line = reader.readLine();
+            }   
+            reader.close();
+        }
+        //System.out.println("\n\n Statistics:");
+        //System.out.println(file.getStatisticCenter().status());
+
+        System.out.println("Numero de registros: "+file.cardinality());
+        System.out.println("Numero de páginas: "+file.size());
+        System.out.println("Numero de bytes: "+file.length());
+        file.close();
+    }
+
+    public static void teste() throws IOException {
+        int pageSize = 4096;
+        int qtdBuckets = 5;
+
+        StaticHashFile file = new StaticHashFile(new DefaultStatisticCenter(),
+                "staticHashFile.hfl", pageSize, qtdBuckets,
+                new IntegerField("sid"),
+                new StringField("sname", 300),
+                new DoubleField("income"));
+
+        
+        file.open();
+        
+        
+        if (file.length() == 0) {//if the file does not exist yet
+            file.insert(file.createRecord(1, "João B. Rocha-Junior", 2304.5));
+            file.insert(file.createRecord(2, "Adélia Ico", 5304.5));
+            file.insert(file.createRecord(3, "EdCarlos da Silva Santana", 1500.0));
+            file.insert(file.createRecord(4, "Fábio Oliveira", 7804.5));
+            file.insert(file.createRecord(5, "Ricardo Carvalho", 5990.5));
+            file.insert(file.createRecord(6, "Ivonete Maciel", 12700.5));
+            file.insert(file.createRecord(7, "João Paulo", 1500.0));
+            file.insert(file.createRecord(8, "Karine Almeida", 1500.0));
+            file.insert(file.createRecord(9, "Tassalon Silva", 1500.0));
+            file.insert(file.createRecord(10, "Kleverton Moisés Silva", 6400.0));
+            file.insert(file.createRecord(11, "Tiago Novais", 5600.0));
+        }
+
+        //System.out.println("\n\n Statistics:");
+        //System.out.println(file.getStatisticCenter().status());
+
+        //System.out.println("\n\nAll students");
+        //print(file.scan()) ; 
+
+        //System.out.println("\n\nStudents with income equals 6400");
+        //print(file.search("income", 6400.0));
+
+        //System.out.println("\n\nStudents with income greater than 3000");
+        //print(file.range("income", OperatorSet.gt, 3000.0));
+
+        //System.out.println("\n\nStudents with income between 1000 and 1500, inclusive");
+        //print(file.range("income", OperatorSet.gte, 1000.0, OperatorSet.and, OperatorSet.ste, 1500.0));
+
+        //System.out.println("\n\nRemove students with name='João B. Rocha-Junior'");
+        //file.remove("sname", "João B. Rocha-Junior");
+
+        System.out.println("\n\nAll students");
+        print(file.scan());
+
+        file.close();
+
+        System.out.println("\n\n Statistics:");
+        //System.out.println(file.getStatisticCenter().status());
+    }
+    
+    public static void main(String[] args) throws IOException {
+        dublin();
+    }
+    
+}
---
+++ src/file/StaticHashFile.java
@@ -0,0 +1,443 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file;
+
+import field.Field;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.Iterator;
+import operators.comparison.LogicOperator;
+import operators.comparison.OperatorSet;
+import operators.comparison.RelOperator;
+import statistics.StatisticCenter;
+
+/**
+ *
+ * @author douglas
+ */
+public class StaticHashFile {
+    
+    private final byte[] buffer;
+    private final BlockFile[] blockFiles;
+    private final Field[] fields; //the fields of the records stored in the file
+    private final int qtdBuckets;
+    /**
+     * Creates a BlockFile.
+     *
+     * @param fileName the name of the file
+     * @param pageSize the size of the page for this file. All pages in the will
+     * have the same size. The pageSize must be a value above zero.
+     * @param fields the fields in each record of the file. The order in which
+     * the fields are inserted is important.
+     */
+    public StaticHashFile(String fileName, int pageSize, int qtdBuckets, Field... fields) {
+        this(null, fileName, pageSize, qtdBuckets, fields);
+    }
+
+    /**numBucket
+     * Creates a BlockFile.
+     *
+     * @param fileName the name of the file
+     * @param pageSize the size of the page for this file. All pages in the will
+     * have the same size. The pageSize must be a value above zero.
+     * @param statisticCenter the class that will collect statistic information
+     * about the file usage
+     */
+    public StaticHashFile(StatisticCenter statisticCenter, String fileName, int pageSize, int qtdBuckets, Field... fields) {
+        this.blockFiles = new BlockFile[qtdBuckets];
+        for(int i=0;i<qtdBuckets;i++) {
+            this.blockFiles[i] = new BlockFile(i+fileName, pageSize, statisticCenter);
+        }
+        //this.blockFile = new BlockFile(fileName, pageSize, statisticCenter);
+        this.fields = fields;
+        this.buffer = new byte[pageSize];
+        this.qtdBuckets = qtdBuckets;
+    }
+
+    /**
+     * This method returns the file name.
+     *
+     * @return the file name
+     */
+    public String getFileName(int numBucket) {
+        return blockFiles[numBucket].getFileName();
+    }
+
+    /**
+     * This method returns the statistic center of the given file
+     *
+     * @return the statistic center of the given file
+     */
+    public StatisticCenter getStatisticCenter(int numBucket) {
+        return blockFiles[numBucket].getStatisticCenter();
+    }
+
+    /**
+     * This method prepares the file to run the operations.
+     *
+     * @throws IOException
+     */
+    public void open() throws IOException {
+        for(int numBucket=0;numBucket<qtdBuckets;numBucket++) {
+            if (new File(this.blockFiles[numBucket].getFileName() + ".mtd").exists()) {
+                ObjectInputStream input;
+                input = new ObjectInputStream(new FileInputStream(this.blockFiles[numBucket].getFileName() + ".mtd"));
+                try {
+                    Field[] storedFields = (Field[]) input.readObject();
+                    if (!Arrays.equals(fields, storedFields)) {
+                        throw new IOException("Fields specifications mismatch!");
+                    }
+                } catch (ClassNotFoundException ex) {
+                    throw new IOException(ex);
+                }
+                input.close();
+            }
+            this.blockFiles[numBucket].open();
+        }
+    }
+
+    /**
+     * This method creates a Record from the given values, using the file fields
+     * definition.
+     *
+     * @param values the values of the record
+     * @return the record with the given values that follows the fields
+     * definition
+     */
+    public Record createRecord(Comparable... values) {
+        if (values.length != fields.length) {
+            throw new RuntimeException("The number of values must be the same"
+                    + " of the number or fields in the record's file!");
+        }
+        Record rec = new Record();
+        Field field;
+        for (int i = 0; i < fields.length; i++) {
+            field = (Field) fields[i].clone();
+            field.setValue(values[i]);
+            rec.add(field);
+        }
+        return rec;
+    }
+
+    /**
+     * This method inserts a record in the last page of the file
+     *
+     * @param record the record to be inserted
+     * @throws IOException
+     */
+    public void insert(Record record) throws IOException {
+        int numBucket = this.hash((int) record.get("id").getValue()) % this.qtdBuckets;
+        long lastPageId = this.blockFiles[numBucket].size();
+        Page page;
+        if (lastPageId == 0) { //the file is empty, first page!
+            page = new Page(this.blockFiles[numBucket].getBlockSize(), fields);
+            lastPageId++;
+        } else {
+            this.blockFiles[numBucket].read(lastPageId, buffer); //read the last page from the disk
+            page = Page.createPage(buffer, fields);
+            if (page.isFull()) { //if the page is full, creates a new empty page
+                page = new Page(this.blockFiles[numBucket].getBlockSize(), fields);
+                lastPageId++;
+            }
+        }
+        page.insert(record); //modifies the page
+        this.blockFiles[numBucket].write(lastPageId, page.toByteArray()); //stores back in the disk
+    }
+
+    /**
+     * This method removes all records that match the fieldName and value. This
+     * method leaves spaces in the blocks.
+     *
+     * @param fieldName the field name to be compared with
+     * @param value the value that satisfies the search criteria
+     * @throws IOException
+     */
+    public void remove(String fieldName, Comparable value) throws IOException {
+        int numBucket = this.hash( (int) value) % this.qtdBuckets;
+        Page page;
+        for (int i=1;i<=this.blockFiles[numBucket].size();i++) {            
+            this.blockFiles[numBucket].read(i, buffer);
+            page = Page.createPage(buffer, fields);
+            if(page.remove(fieldName, value)){
+                this.blockFiles[numBucket].write(i, page.toByteArray());
+            }            
+        }
+    }
+
+    /**
+     * This method searches for all records whose the value of the given field
+     * name is equals to the value parameter
+     *
+     * @param fieldName the field name to be compared with
+     * @param value the value that satisfies the search criteria
+     * @return the list of records that satisfies the search criteria
+     * @throws IOException
+     */
+    public Iterator<Record> search(String fieldName, Comparable value) throws IOException {
+        return range(fieldName, OperatorSet.eq, value);
+    }
+
+    /**
+     * This method searches for all records whose the value of the given field
+     * name satisfies the relational operator op
+     *
+     * @param fieldName the field name to be compared with
+     * @param op the relational operator (<,<=, =, >,>= e !=)
+     * @param value the value that satisfies the search criteria
+     * @return the list of records that satisfies the search criteria
+     * @throws IOException
+     */
+    public Iterator<Record> range(String fieldName, RelOperator op, Comparable value) throws IOException {
+        return range(fieldName, op, value, null, null, null);
+    }
+
+    /**
+     * This method returns records whose the given field's value attends both
+     * search criteria
+     *
+     * @param fieldName the field name to be compared with
+     * @param firstOp the first (left) relational operator (<,<=, =, >,>= e !=)
+     * @param firstValue the first (left) value that satisfies the search
+     * criteria
+     * @param logicOp the logic operator (And or Or)
+     * @param secondOp the second (right) relational operator (<,<=, =, >,>= e
+     * !=)
+     * @param secondValue the second (right) value that satisfies the search
+     * criteria
+     * @return the list of records that satisfies the search criteria
+     * @throws IOException
+     */
+    public Iterator<Record> range(String fieldName, RelOperator firstOp, Comparable firstValue,
+            LogicOperator logicOp, RelOperator secondOp, Comparable secondValue) throws IOException {
+        return new RangeIterator(scan(), fieldName, firstOp, firstValue,
+                logicOp, secondOp, secondValue);
+    }
+
+    /**
+     * This method returns an iterator over all records in the file
+     *
+     * @return the iterator over all records of the file
+     * @throws IOException
+     */
+    public Iterator<Record> scan() throws IOException {
+        return new ScanIterator();
+    }
+
+    /**
+     * This method returns the number of records stored in the file. This method
+     * performs a scan in the file in order to figure out the number of records.
+     *
+     * @return the number of records stored in the file.
+     */
+    public long cardinality() throws IOException {
+        long count = 0;
+        Iterator it = scan();
+        while (it.hasNext()) {
+            count++;
+            it.next();
+        }
+        return count;
+    }
+    
+    /**
+     * This method returns the number of blocks (pages) stored in the file.
+     *
+     * @return the number of blocks stored in the file
+     * @throws IOException if there is an I/O problem during the operation
+     */
+    public long size() throws IOException {
+       long totalSize = 0;
+       for(int numBucket=0;numBucket<qtdBuckets;numBucket++)
+           totalSize += this.blockFiles[numBucket].size();
+       return totalSize;
+    }
+
+    /**
+     * This method returns the size of the file in bytes.
+     *
+     * @return the size of the file in bytes
+     * @throws IOException if there is an I/O problem during the operation
+     */
+    public long length() throws IOException {
+        long length = 0;
+        for(int i=0;i<qtdBuckets;i++)
+            length += this.blockFiles[i].length();
+        return length;
+    }
+
+    /**
+     * This method writes the file metadata
+     *
+     * @throws IOException
+     */
+    public void close() throws IOException {
+        for(int numBucket=0;numBucket<qtdBuckets;numBucket++) {
+            if (!new File(this.blockFiles[numBucket].getFileName() + ".mtd").exists()) {
+                ObjectOutputStream output;
+                output = new ObjectOutputStream(new FileOutputStream(this.blockFiles[numBucket].getFileName() + ".mtd"));
+                output.writeObject(fields);
+                output.close();
+            }
+            this.blockFiles[numBucket].close();
+        }
+    }
+
+    /**
+     * This method iterates of the records that attends a search criteria
+     */
+    private class RangeIterator implements Iterator<Record> {
+
+        private final Iterator<Record> scan;
+        private final String fieldName;
+        private final Comparable firstValue, secondValue;
+        private final RelOperator firstOp, secondOp;
+        private final LogicOperator logicOp;
+        private Record nextRec;
+
+        public RangeIterator(Iterator<Record> scan, String fieldName,
+                RelOperator firstOp, Comparable firstValue,
+                LogicOperator logicOp, RelOperator secondOp, Comparable secondValue) {
+            this.scan = scan;
+            this.fieldName = fieldName;
+            this.firstOp = firstOp;
+            this.firstValue = firstValue;
+            this.logicOp = logicOp;
+            this.secondOp = secondOp;
+            this.secondValue = secondValue;
+
+            nextRec = getNextRecord();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextRec != null;
+        }
+
+        @Override
+        public Record next() {
+            Record retRec = nextRec;
+            nextRec = getNextRecord();
+            return retRec;
+        }
+
+        private Record getNextRecord() {            
+            while (scan.hasNext()) {
+                Record rec = scan.next();
+                Comparable fieldValue = rec.get(fieldName).getValue();
+
+                if (logicOp == null) { //checks only th left side (first operator and value)
+                    if (firstOp.compare(fieldValue, firstValue)) {                        
+                        return rec; //it has found a record that attends the query
+                    }
+                } else {
+                    if (logicOp.compare(firstOp.compare(fieldValue, firstValue),
+                            secondOp.compare(fieldValue, secondValue))) {                        
+                        return rec; //it has found a record that attends the query
+                    }
+                }                
+            }
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+    /**
+     * Scans the heap file
+     */
+    private class ScanIterator implements Iterator<Record> {
+
+        private long numPages = 0;
+        private byte[] buffer;
+        private int currentPageId = 0;
+        private Page currentPage;
+        private Iterator<Record> recIterator;
+        private int numBucket;
+
+        public ScanIterator() throws IOException {
+            this.numBucket = 0;
+            buffer = new byte[blockFiles[numBucket].getBlockSize()];
+            numPages = blockFiles[numBucket].size();
+            recIterator = nextPageIterator();
+        }
+
+        private Iterator<Record> nextPageIterator() {
+            try {
+                Iterator<Record> it = null;
+                while (currentPageId < numPages || numBucket < qtdBuckets) {
+                    if( numPages == 0 || currentPageId == numPages) {
+                        numBucket++;
+                        if(numBucket >= qtdBuckets) {
+                            return null;
+                        }
+                        currentPageId = 0;
+                        numPages = blockFiles[numBucket].size();
+                        buffer = new byte[blockFiles[numBucket].getBlockSize()];
+                        continue;
+                    }
+                    currentPageId++;
+                    blockFiles[numBucket].read(currentPageId, buffer);
+                    currentPage = Page.createPage(buffer, fields);
+                    it = currentPage.iterator();
+                    if (it.hasNext()) { //checks if there is records in the page
+                        break; //leave the loop
+                    } else {
+                        numBucket++;
+                        currentPageId = 0;
+                        numPages = blockFiles[numBucket].size();
+                        buffer = new byte[blockFiles[numBucket].getBlockSize()];
+                        it = null;
+                    }
+                }
+                return it;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return recIterator != null && recIterator.hasNext();
+        }
+
+        @Override
+        public Record next() {
+            Record data = null;
+            if (recIterator != null) {
+                data = recIterator.next();
+
+                if (!recIterator.hasNext()) {
+                    recIterator = nextPageIterator();
+                }
+            }
+            return data;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+    /**
+     * Função hash usada ( a * value ) + b
+     * @param value
+     * @return 
+     */
+    public int hash(int value) {
+        int a = 7;
+        int b = 9;
+        return (a * value) + b;
+    }
+}
